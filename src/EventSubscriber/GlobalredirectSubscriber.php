6c845e73e77f6d66719d2e36ceaaa3382d182c02
Sascha Grossenbacher <saschagros@gmail.com>
2015-02-26 23:42:38 +0100

    Use new entity: uri for entity based redirection.

--- src/EventSubscriber/GlobalredirectSubscriber.php
+++ src/EventSubscriber/GlobalredirectSubscriber.php
@@ -193,8 +193,7 @@ class GlobalredirectSubscriber implements EventSubscriberInterface {
 
     $term = $this->entityManager->getStorage('taxonomy_term')->load($matches[1]);
     if (!empty($term) && $term->url() != $request->getPathInfo()) {
-      $system_path = $this->aliasManager->getPathByAlias(ltrim($term->url(), '/'));
-      $this->setResponse($event, Url::fromUri('internal:' . $system_path));
+      $this->setResponse($event, Url::fromUri('entity:taxonomy_term/' . $term->id()));
     }
   }
 
